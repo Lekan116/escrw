07cadf0645d75f3cd8f987843cf0f162c7c4c4a6
Lekan116 <canapaypal51@gmail.com>
2025-12-25 16:50:18 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,14 +3,11 @@ from threading import Thread
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Escrow Bot is alive", 200
+    return "P2P EscrowBot is running"
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    thread = Thread(target=run)
-    thread.daemon = True
-    thread.start()
+    Thread(target=lambda: app.run(host="0.0.0.0", port=8080)).start()
